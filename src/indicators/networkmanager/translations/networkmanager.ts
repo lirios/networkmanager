ef0a017338a8d86cde9c0c24eee2b1978df18fa3
Pier Luigi Fiorini <pierluigi.fiorini@liri.io>
2018-05-29 08:35:02 +0200

    Add indicator source translation

---
+++ src/indicators/networkmanager/translations/networkmanager.ts
@@ -0,0 +1,53 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>AirplaneMode</name>
+    <message>
+        <location filename="../contents/AirplaneMode.qml" line="42"/>
+        <source>Airplane mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ConnectionItem</name>
+    <message>
+        <location filename="../contents/ConnectionItem.qml" line="78"/>
+        <source>Connected</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../contents/ConnectionItem.qml" line="88"/>
+        <source>Connect to Network</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../contents/ConnectionItem.qml" line="97"/>
+        <source>Connect</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../contents/ConnectionItem.qml" line="105"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../contents/ConnectionItem.qml" line="128"/>
+        <source>Password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../contents/ConnectionItem.qml" line="145"/>
+        <source>Show password</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../contents/main.qml" line="34"/>
+        <source>Network</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
