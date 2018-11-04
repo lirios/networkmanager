46775a6340789cc0fbe269db0c2875878e98bed4
Pier Luigi Fiorini <pierluigi.fiorini@liri.io>
2018-11-04 09:06:16 +0100

    Use Fedora for the CI
    KDE Neon on 16.04 is EOL, better migrate to a supported distribution.

--- .travis/build.sh
+++ .travis/build.sh
@@ -7,12 +7,12 @@ source /usr/local/share/liri-travis/functions
 # Install dependencies
 travis_start "install_packages"
 msg "Install packages..."
-sudo apt-get install -y \
-     libnm-glib-dev \
-     libkf5networkmanagerqt-dev \
-     libmm-glib-dev \
-     modemmanager-dev \
-     modemmanager-qt-dev
+dnf install -y \
+     NetworkManager-libnm-devel \
+     ModemManager-devel \
+     ModemManager-glib-devel \
+     kf5-networkmanager-qt-devel \
+     kf5-modemmanager-qt-devel
 travis_end "install_packages"
 
 # Install artifacts
@@ -36,7 +36,7 @@ dbus-run-session -- \
 xvfb-run -a -s "-screen 0 800x600x24" \
 qbs -d build -j $(nproc) --all-products profile:travis-qt5 \
     modules.lirideployment.prefix:/usr \
-    modules.lirideployment.libDir:/usr/lib/x86_64-linux-gnu \
-    modules.lirideployment.qmlDir:/usr/lib/x86_64-linux-gnu/qt5/qml \
-    modules.lirideployment.pluginsDir:/usr/lib/x86_64-linux-gnu/qt5/plugins
+    modules.lirideployment.libDir:/usr/lib64 \
+    modules.lirideployment.qmlDir:/usr/lib64/qt5/qml \
+    modules.lirideployment.pluginsDir:/usr/lib64/qt5/plugins
 travis_end "build"
