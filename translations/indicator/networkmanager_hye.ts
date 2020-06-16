45cf8da7100d4a6702c37e4e4f65eea040a9028b
Liri CI <ci@liri.io>
2020-06-16 00:23:15 +0000

    Automatic merge of Transifex translations
    [ci skip]

---
+++ translations/indicator/networkmanager_hye.ts
@@ -0,0 +1,53 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="hye" version="2.1">
+<context>
+    <name>Connection</name>
+    <message>
+        <location filename="../../src/indicators/networkmanager/contents/Connection.qml" line="50"/>
+        <source>%1, %2</source>
+        <translation>%1, %2</translation>
+    </message>
+    <message>
+        <location filename="../../src/indicators/networkmanager/contents/Connection.qml" line="53"/>
+        <source>Connected</source>
+        <translation>Կապակցուած է</translation>
+    </message>
+    <message>
+        <location filename="../../src/indicators/networkmanager/contents/Connection.qml" line="83"/>
+        <source>Connect to Network</source>
+        <translation>Կապակցուած է ցանցին</translation>
+    </message>
+    <message>
+        <location filename="../../src/indicators/networkmanager/contents/Connection.qml" line="92"/>
+        <source>Connect</source>
+        <translation>Մինանալ</translation>
+    </message>
+    <message>
+        <location filename="../../src/indicators/networkmanager/contents/Connection.qml" line="100"/>
+        <source>Cancel</source>
+        <translation>Չեղարկել</translation>
+    </message>
+    <message>
+        <location filename="../../src/indicators/networkmanager/contents/Connection.qml" line="123"/>
+        <source>Password</source>
+        <translation>Գաղտնաբառ</translation>
+    </message>
+    <message>
+        <location filename="../../src/indicators/networkmanager/contents/Connection.qml" line="139"/>
+        <source>Show password</source>
+        <translation>Ցոյց տալ գաղտնաբառը</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../../src/indicators/networkmanager/contents/main.qml" line="34"/>
+        <source>Network</source>
+        <translation>Ցանց</translation>
+    </message>
+    <message>
+        <location filename="../../src/indicators/networkmanager/contents/main.qml" line="43"/>
+        <source>Use Wi-Fi</source>
+        <translation>ԱւգտագործելWi-Fi</translation>
+    </message>
+</context>
+</TS>
